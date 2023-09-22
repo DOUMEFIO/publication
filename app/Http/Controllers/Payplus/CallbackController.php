8104776db31dc8daed84ccd2e8924acf6e5e520b
Stephane ASSOCLE <mensste30@gmail.com>
2023-09-22 12:27:42 +0100

    whatsapp-verification callback from payplus

---
+++ app/Http/Controllers/Payplus/CallbackController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Http\Controllers\Payplus;
+
+use App\Http\Controllers\Controller;
+
+class CallbackController extends Controller
+{
+    function confirmWhatsappNumber()
+    {
+        //....
+    }
+}
