81fe7a50e4f2ce9a6cd23e76b2f54c3ddf3741c0
DOUMEFIO <108420187+DOUMEFIO@users.noreply.github.com>
2023-10-12 15:01:18 +0200

    send whassp finish

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -6,7 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Carbon\Carbon;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Psr7\Request;
+use App\Models\SendMessage;
 
 class Admin extends Model
 {
@@ -14,41 +16,29 @@ class Admin extends Model
     public static function sendtachewhatsap($id){
         $tachedo = Tache::with('type')->where('id',$id)->first();
         $tacheUser = TravailleurTache::where('idTache',$id)->pluck('idtravailleur')->toArray();
-        $tacheinflu = InfoInfluenceur::whereIn('id_User',$tacheUser)->pluck('tel')->toArray();
+        $tacheinflu = InfoInfluenceur::whereIn('id_User',$tacheUser)->pluck("tel")->toArray();
         $dateDebut = Carbon::parse($tachedo->debut);
         $debutFormatee = $dateDebut->isoFormat('dddd D MMMM YYYY', 'Do MMMM YYYY');
         $dateFin = Carbon::parse($tachedo->debut);
         $finFormatee = $dateFin->isoFormat('dddd D MMMM YYYY', 'Do MMMM YYYY');
         if ($tachedo->description){
-            $message = "Bonjour1 monsieur voici vos nouveau tâche qui début le ".$debutFormatee. " et prend fin le ".$finFormatee. " C'est un ".$tachedo->type->libelle. " La description est <strong> $tachedo->description</strong>";
+            $message = "Bonjour monsieur voici votre nouvelle tâche qui débute le $debutFormatee et prend fin le $finFormatee.
+    C'est un ".$tachedo->type->libelle.". Le contenue est *$tachedo->description*.";
         } else {
-            $message = "Bonjour monsieur voici vos nouveau tâche qui début le ".$debutFormatee. " et prend fin le ".$finFormatee. " C'est un ".$tachedo->type->libelle.". Il y a pas de description." ;
+            $message = "Bonjour monsieur voici votre nouvelle tâche qui débute le $debutFormatee et prend fin le $finFormatee.
+    C'est un ".$tachedo->type->libelle.". Il y a pas de contenu." ;
         }
-        if ($tachedo->fichier){
-            $fichier = "http://publication.lapieuvretechnologique.info/storage".$tachedo->fichier;
-        } else {
-            $fichier = "" ;
-        }
+        $fichier = "http://publication.lapieuvretechnologique.info/storage".$tachedo->fichier;
+
         $message = $message;
-        //dd($message, $tachedo->description, $fichier,$tacheinflu);
-        $client = new Client();
-        $headers = [
-        'Authorization' => '01da56df-8699-483d-96a1-d4f3675b1ede',
-        'Apikey' => '0510efde-39e9-4927-a440-f9029d5997f2',
-        'Content-Type' => 'application/json'
-        ];
-        foreach ($tacheinflu as $destinataire) {
-            $destinataire = preg_replace('/\D/', '', $destinataire);
-            $body = [
-                "receiver" => $destinataire,
-                "media" => $fichier,
-                "message" => $message,
-                "callback_url" => "",
-            ];
-            $request = new Request('POST', 'http://51.161.128.10:3366/api/send_message', $headers, $body);
-            $res = $client->sendAsync($request)->wait();
-            echo $res->getBody();
+
+        if(!blank($tachedo->fichier) && !blank($tachedo->description)){
+            SendMessage::message($tacheinflu,$message);
+            SendMessage::media($tacheinflu,$fichier);
+        } elseif(!blank($tachedo->fichier) && blank($tachedo->description)) {
+            SendMessage::media($tacheinflu,$fichier);
+        } else{
+            SendMessage::message($tacheinflu,$message);
         }
     }
-
 }
